12cf7fce91f6f5d609c36236d968730765b230f1
wanwu <wanwu@microsoft.com>
2020-05-05 10:07:21 +0800

    Update database_dao.py

--- BuyService/CosmosDB/database_dao.py
+++ BuyService/CosmosDB/database_dao.py
@@ -13,11 +13,9 @@ class DatabaseDAO:
             offer_throughput=400
         )
 
-    def select_by_name(self,name):
-        return list(self.container.query_items(
-            query="SELECT * FROM c where c.name CONTAINS" + name,
-            enable_cross_partition_query=True
-        ))
+    def select_by_name(self, name):
+        query="SELECT * FROM c where c.name CONTAINS('" + name+"')"
+        return self.select_items(query)
 
     def add_item(self, item_content):
         self.container.create_item(body=item_content)
